7e6445b057fc55daa9e9802606bc507ff9f4d2d9
andriy101 <andriy101@gmail.com>
2019-03-25 02:55:59 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+### My interactive resume
+
+[![npm version](https://img.shields.io/npm/v/andriy-resume.svg?style=flat)](https://www.npmjs.com/package/andriy-resume)
+[![npm](https://img.shields.io/npm/dt/andriy-resume.svg)](https://npm-stat.com/charts.html?package=andriy-resume)
+
+This project is inspired by [How to make your Résumé an NPM package](https://blog.usejournal.com/how-to-make-your-r%C3%A9sum%C3%A9-an-npm-package-fc5d6b6a3fbd) article.
+
+ASCII text is generated with [Text to ASCII art generator](http://patorjk.com/software/taag/).
+
+### View resume
+
+Simply run npx command:
+
+```bash
+npx andriy-resume
+```
+
+### TODOS
+* Add tests
---
+++ index.js
@@ -0,0 +1,89 @@
+#!/usr/bin/env node
+'use strict';
+
+const names = require('./ascii-art');
+
+const inquirer = require('inquirer');
+const chalk = require('chalk');
+
+const title = chalk.bold.green;
+const response = chalk.italic.green;
+
+const resume = require('./resume.json');
+
+
+const resumePrompts = {
+  type: 'list',
+  name: 'resumeOptions',
+  message: 'What would do you like to discover about me?',
+  choices: [...Object.keys(resume), 'Exit']
+};
+
+function main() {
+  const greeting = 'Hello, my name is Andriy Malish and welcome to my resume\n';
+  if (process.stdout.columns > 111) {
+    let index = 0;
+    const interval = setInterval(_ => {
+      console.clear(); 
+      console.log(chalk.yellow(names[++index % 2]));
+
+      if (index === 20) {
+        clearInterval(interval);
+        console.log(greeting);
+        resumeHandler();
+      }
+    }, 200);
+  }
+  else {
+    console.clear(); 
+    console.log(greeting);
+    resumeHandler();
+  }
+}
+
+function resumeHandler() {
+  inquirer.prompt(resumePrompts).then(answer => {
+    if (answer.resumeOptions == 'Exit') {
+      return;
+    }
+    const option = answer.resumeOptions;
+
+    console.log(response('--------------------------------------'));
+
+    resume[`${option}`].forEach((info, ind) => {
+      if (typeof info === 'string') {
+        console.log(response(`  ${info}`));
+      }
+      else {
+        Object.values(info).forEach((value, ind) => {
+          if (ind) {
+            console.log(response(`    ${value}`));
+          }
+          else {
+            console.log(title(`  ${value.padEnd(38)}`));
+          }
+        });
+        ind !== resume[`${option}`].length - 1 && console.log(response('--------------------------------------'));
+      }
+    });
+    
+    console.log(response('--------------------------------------'));
+
+    inquirer
+      .prompt({
+        type: 'list',
+        name: 'exitBack',
+        message: 'Go back or Exit?',
+        choices: ['Back', 'Exit']
+      })
+      .then(choice => {
+        if (choice.exitBack == 'Back') {
+          resumeHandler();
+        } else {
+          return;
+        }
+      });
+  });
+}
+
+main();
